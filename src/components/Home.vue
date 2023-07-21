77a423ff59514a391fa2d61f29d4ed7c3c612b1a
sunmantra <zcernyh14@gmail.com>
2023-07-21 17:16:31 +0700

    addelemcatalog

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,10 +7,44 @@
             <div class="container">
                 <h1 class="crownchoice__title"> THE CROWN FOR YOUR KITCHEN</h1>
                 <div class="crownchoice__element">
-                    <p>Reginox bv, Nederland</p>
-                    <p>Reginox Rif Holding, Russia</p>
-                    <p>Reginox, UK Ltd</p>
-                    <p>Reginox, Singapure</p>
+                    <div class="dropdown">
+                        <button class="dropbtn"> Reginox bv, Nederland<img src="../img/Vector.svg" alt=""></button>
+                        <div class="dropdown-content">
+                            <p href="#">1 Loyang Lane #03-03 Singapore 508913</p>
+                            <p href="#">+ 65 (0) 67 88 53 63</p>
+                            <p href="#">info@reginox.com.sg</p>
+                            <p href="#">reginox.sg</p>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <button class="dropbtn"> Reginox Rif Holding, Russia <img src="../img/Vector.svg" alt=""></button>
+                        <div class="dropdown-content">
+                            <p href="#">1 Loyang Lane #03-03 Singapore 508913</p>
+                            <p href="#">+ 65 (0) 67 88 53 63</p>
+                            <p href="#">info@reginox.com.sg</p>
+                            <p href="#">reginox.sg</p>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <button class="dropbtn">Reginox, UK Ltd<img src="../img/Vector.svg" alt=""></button>
+                        <div class="dropdown-content">
+                            <p href="#">1 Loyang Lane #03-03 Singapore 508913</p>
+                            <p href="#">+ 65 (0) 67 88 53 63</p>
+                            <p href="#">info@reginox.com.sg</p>
+                            <p href="#">reginox.sg</p>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <button class="dropbtn">Reginox, Singapure<img src="../img/Vector.svg" alt=""></button>
+                        <div class="dropdown-content">
+                            <p href="#">1 Loyang Lane #03-03 Singapore 508913</p>
+                            <p href="#">+ 65 (0) 67 88 53 63</p>
+                            <p href="#">info@reginox.com.sg</p>
+                            <p href="#">reginox.sg</p>
+                        </div>
+                    </div>
+                    
+
                 </div>
             </div> 
         </div>
@@ -18,41 +52,170 @@
             <div class="container">
                 <h2 style="margin-top: 132px;">КАТАЛОГ</h2>
                 <div style="height: 1420px;" class="catalog__group">
-                    <div class="catalog__elem" >1<img src="../img/group/1.png" alt=""> </div>
-                    <div class="catalog__elem">2<img src="../img/group/2.png" alt=""></div>
-                    <div class="catalog__elem">3<img src="../img/group/3.png" alt=""></div>
-                    <div class="catalog__elem">4<img src="../img/group/4.png" alt=""></div>
-                    <div class="catalog__elem">5<img src="../img/group/5.png" alt=""></div>
-                    <div class="catalog__elem">6<img src="../img/group/6.png" alt=""></div>
-                    <div class="catalog__elem">4<img src="../img/group/7.png" alt=""></div>
-                    <div class="catalog__elem">5<img src="../img/group/8.png" alt=""></div>
-                    <div class="catalog__elem">6<img src="../img/group/9.png" alt=""></div>
+                    <div class="catalog__elem" ><img src="../img/group/1.png" alt=""> </div>
+                    <div class="catalog__elem"><img src="../img/group/2.png" alt=""></div>
+                    <div class="catalog__elem"><img src="../img/group/3.png" alt=""></div>
+                    <div class="catalog__elem"><img src="../img/group/4.png" alt=""></div>
+                    <div class="catalog__elem"><img src="../img/group/5.png" alt=""></div>
+                    <div class="catalog__elem"><img src="../img/group/6.png" alt=""></div>
+                    <div class="catalog__elem"><img src="../img/group/7.png" alt=""></div>
+                    <div class="catalog__elem"><img src="../img/group/8.png" alt=""></div>
+                    <div class="catalog__elem"><img src="../img/group/9.png" alt=""></div>
                 </div>
                 <h2 style="margin-top: 130px;">ПОПУЛЯРНЫЕ ТОВАРЫ</h2>
                 <div style="height: 1000px;" class="catalog__group">
-                    <div class="catalog__elem">1<img src="../img/group/1.png" alt=""> </div>
-                    <div class="catalog__elem">2<img src="../img/group/2.png" alt=""></div>
-                    <div class="catalog__elem">3<img src="../img/group/3.png" alt=""></div>
-                    <div class="catalog__elem">4<img src="../img/group/4.png" alt=""></div>
-                    <div class="catalog__elem">5<img src="../img/group/5.png" alt=""></div>
-                    <div class="catalog__elem">6<img src="../img/group/6.png" alt=""></div>
+                    <div class="catalog__elem"><div class="catalog__nameel"><img style="margin-top:25px ;" src="../img/pop/1.svg" alt="">  <div><h3>Amsterdam 25 Dark chocolate</h3><p>9 950 руб</p></div></div> </div>
+                    <div class="catalog__elem"><div class="catalog__nameel"><img style="margin-top:25px ;" src="../img/pop/2.png" alt="">  <div><h3>YADKIN K1065K black</h3><p>31 000 руб                  </p></div></div> </div>
+                    <div class="catalog__elem"><div class="catalog__nameel"><img style="margin-top:25px ;" src="../img/pop/3.png" alt="">  <div><h3>Diplomat R 20 lux</h3><p>17 000 руб</p></div></div> </div>
+                    <div class="catalog__elem"><div class="catalog__nameel"><img style="margin-top:25px ;" src="../img/pop/4.png" alt="">  <div><h3>Столешница WAFELBLAD R00939</h3><p>47 520 руб</p></div></div> </div>
+                    <div class="catalog__elem"><div class="catalog__nameel"><img style="margin-top:25px ;" src="../img/pop/5.png" alt="">  <div><h3>Amsterdam 34 Caffe Silvery</h3><p>19 000 руб</p></div></div> </div>
+                    <div class="catalog__elem"><div class="catalog__nameel"><img style="margin-top:25px ;" src="../img/pop/6.png" alt="">  <div><h3>R18 370 OSP lux</h3><p>7 300 руб</p></div></div> </div>
                 </div>
                 <h2 style="margin-top: 205px;">О КОМПАНИИ</h2>
                 <div  class="about">
                     <h3 style="font-weight: 600;">Добро пожаловать на сайт Компании Reginox Reef Holding(ООО «РеджиноксРиф   Холдинг») — единственного и официального   представителя производителя сантехнической продукции для кухни и ванной комнаты Reginox (Нидерланды).</h3>
                     <h3 >Основной специализацией нашей компании являются розничные и оптовые продажи кухонной сантехники по всей России. Мы предлагаем кухонные мойки из нержавеющей стали и гранитного композита,  смесители для кухни, а также оригинальную продукцию от Reginox:</h3>
-                    <h3>умывальники окрашенныемойки системы газовых конфорок  PITTbyReginox многофункциональные кухонные комплексы и др.</h3>
+                    <div class="about__list">
+                        <div class="about__el">
+                            <div class="about__ball"></div>
+                            <h3>умывальники</h3>
+                        </div>
+                        <div class="about__el">
+                            <div class="about__ball"></div>
+                            <h3>окрашенныемойки</h3>
+                        </div>
+                        <div class="about__el">
+                            <div class="about__ball"></div>
+                            <h3>системы газовых конфорок PITTbyReginox</h3>
+                        </div>
+                        <div class="about__el">
+                            <div class="about__ball"></div>
+                            <h3>многофункциональные кухонные комплексы и др.</h3>
+                        </div>
+                    </div>
                     <h3>Более 25 лет назад компания RRHolding представила на российском рынке кухонные мойки Reginox из нержавеющей стали, приятно удивившие покупателей отличным качеством, многообразием моделей и современным дизайном. Продукция Reginox входит в постоянный ассортимент ряда крупных фабрик-производителей мебели, введена в предложение крупнейших продавцов мебельной фурнитуры и комплектующих, и представлена вомножестве кухонных салонов по всей России. За годы нашей работы мы заключили контракты в сферах: </h3>
-                    <h3>авиастроение вагоностроение</h3>
-                    <h2 style="font-size: 32px;">Наши преимущества</h2>
-                    <h3>список</h3>
+                    <h3> </h3>
+                    <div class="about__list">
+                        <div class="about__el">
+                            <div class="about__ball"></div>
+                            <h3>авиастроение</h3>
+                        </div>
+                        <div class="about__el">
+                            <div class="about__ball"></div>
+                            <h3>вагоностроение</h3>
+                        </div>
+                        <div class="about__el">
+                            <div class="about__ball"></div>
+                            <h3>судостроение</h3>
+                        </div>
+                        <div class="about__el">
+                            <div class="about__ball"></div>
+                            <h3>здравоохранение</h3>
+                        </div>
+                        <div class="about__el">
+                            <div class="about__ball"></div>
+                            <h3>предприятия общественного питания </h3>
+                        </div>
+                        <div class="about__el">
+                            <div class="about__ball"></div>
+                            <h3>иные учреждения сферы обслуживания и др.</h3>
+                        </div>
+                    </div>
+                    <h2 style="font-size: 32px; margin-top: 50px;">Наши преимущества</h2>
+                    <div class="about__list">
+                        <div class="about__el">
+                            <div class="about__ball"></div>
+                            <h3>многолетний   опыт, профессиональный подход, надежное партнерство и отличная репутация на рынке</h3>
+                        </div>
+                        <div class="about__el">
+                            <div class="about__ball"></div>
+                            <h3>высококачественный европейский товар и гарантия производителя, соответствие всем международным и российским стандартам и требованиям</h3>
+                        </div>
+                        <div class="about__el">
+                            <div class="about__ball"></div>
+                            <h3>постоянное наличие на складе Москвы и Санкт-Петербурга (официальное представительство ООО «Ревестал»), а также возможность изготовления под индивидуальный заказ</h3>
+                        </div>
+                        <div class="about__el">
+                            <div class="about__ball"></div>
+                            <h3>работа с розничными покупателями, с мелкооптовыми и крупнооптовыми заказчиками, сотрудничество с каждым покупателем или бизнес-партнеромпо специальной программе и индивидуальной ценовой политике</h3>
+                        </div>
+                        <div class="about__el">
+                            <div class="about__ball"></div>
+                            <h3>широкий ассортимент и различные модификации, позволяющие реализоватьпроекты в различных сферах- доставка в любую точку России</h3>
+                        </div>
+                    </div>
                     <h3>Благодаря многолетнему и плодотворному тандему RRHolding и Reginox, сегодня мы имеем большое количество бизнес-партнеров, в том числе и за пределами РФ, с которыми поддерживаем и развиваем взаимовыгодные отношения, приобрели популярность среди потребителей, и сохранили за собой статус компании, предлагающей на рынке России продукцию со «стальной репутацией». Мы предлагаем лучшее качество от европейского завода-производителя (завод   расположен   в   Нидерландах,   город   Rijssen) и пожизненную гарантию на продукцию из нержавеющей стали!</h3>
+                </div>
+                <div class="footer">
+                    <div class="footer__pillar">
+                        <h3>О компании</h3>
+                       <p> Почему мы</p>
+                       <p>Наши партнеры</p>
+                       <p>Сертификаты и гарантии</p>
+                       <p>Статьи</p>
+                       <p>Информация</p>
+                       <p>Каталоги</p>
+                       <p>Доставка и оплата</p>
+                       <p>Контакты</p>
+                    </div>
+                    <div class="footer__pillar">
+                        <h3>Каталог</h3>
+                       <p>Мойки </p>
+                       <p>Смесители</p>
+                       <p>Аксессуары</p>
+                       <p>Плиты</p>
+                       <p>Умывальники</p>
+                       <p>Столешницы</p>
+                       <p>Системы хранения Manhattan</p>
+                       <p>Измельчители Bone Crusher</p>
+                       <p>Другая продукция</p>
+                    </div>
+                    <div class="footer__pillar">
+                        <h3>Бренды</h3>
+                       <p>Reginox</p>
+                       <p>Rodi</p>
+                       <p>Whinstone</p>
+                       <p>Rerih</p>
+                       <p>Status</p>
+                       <p>Bone Crusher</p>
+                       <p>Armando Vicario</p>
+                       <p>Webert</p>
+                       <p>Effepi</p>
+                       <p>Glionna Bagno</p>
+                       <p>Аквафор</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="flin">
+            <div class="container">
+                <div class="flin__group">
+                    <div style="margin-bottom: 32px;">
+                        <img src="../img/icon/1.png" alt="">
+                        <img style="margin-left: 60px;" src="../img/icon/2.svg" alt="">
+                    </div>
+                    
+                    <img style="margin-bottom: 32px;" src="../img/icon/3.svg" alt="">
+                    <img src="../img/icon/4.png" alt="">
+                </div>
+            </div>
+        </div>
+
+        <div class="polushon"
+             style=" width: 100%;
+             height: 45px;">
+            <div class="container">
+                <div style="display: flex;
+                    align-items: center;
+                    justify-content: space-between;
+                    width: 100%;">
+                    <a href="">Политика конфеденциальности</a>
+                    <p  style="margin: 0;">2020 © Reginox-shop</p>
                 </div>
             </div>
         </div>
     </div>
-   
-
 </template>
 
 <script>
@@ -144,17 +307,167 @@ height: 811px;
     width: 383px;
     height: 440px;
     flex-shrink: 0;
-    background: linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, #000 100%);
+    background:#ECECEC;
 
-    }
-.catalog__elem img{
+    /*background: linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, #000 100%);
+*/}
+.catalog__nameel{
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
 }
-
+.catalog__nameel h3{
+    color: var(--gray-1, #333);
+    font-family: Montserrat;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 800;
+    line-height: 160%; /* 28.8px */
+}
+.catalog__nameel p{
+    color: var(--gray-1, #333);
+font-family: Montserrat;
+font-size: 20px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+}
 .about{
     display: flex;
     width:816px ;
     flex-direction: column;
     align-items: flex-start;
     margin-left: 200px;
+}
+.about__list{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+}
+.about__el{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+.about__el h3{
+    margin: 10px 0 10px 0;
+}
+.about__ball{
+    width: 6px;
+    height: 6px;
+    transform: rotate(90deg);
+    flex-shrink: 0;
+    background: var(--red, #AD2131);  
+    border-radius: 100%;
+    margin: 0 27px 0 27px;
+}
+.dropbtn {
+    padding: 16px;
+    border: none;
+    color: var(--grafit, #333);
+    font-family: Montserrat;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    background: #ECECEC;
+    min-width: 260px;
+    text-align:left;
+    
+}
+.dropbtn img{
+    margin-left: 10px;
+}
+.dropbtn:hover{
+    background: #ffffff;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #ffffff;
+  min-width: 260px;
+  z-index: 1;
+}
+
+.dropdown-content p {
+  text-decoration: none;
+  display: block;
+ padding-left: 15px;
+    margin-top:15px;
+    color: var(--gray, #666565);
+    font-family: Montserrat;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+}
+.dropdown:hover .dropdown-content {display: block;}
+
+.footer{
+    height: 550px;
+    display: flex;
+    max-width: 1000px;
+    justify-content: space-between;
+    margin: 134px 0px 66px 0px;
+}
+.footer h3{
+    color: var(--grafit, #333);
+    font-family: Montserrat;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+}
+.footer p{
+    color: var(--gray, #666565);
+    font-family: Montserrat;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    margin: 12px 0;
+}
+.footer__pillar{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+}
+.flin{
+    width: 100%;
+    height: 225px;
+    background: var(--gray-light, #ECECEC);
+    margin-bottom:30px ;
+    
+}
+.flin__group{
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+}
+.polushon a{
+    color: var(--gray, #666565);
+    font-family: Montserrat;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    text-decoration-line: underline;
+}
+.polushon p{
+    color: var(--gray, #666565);
+    font-family: Montserrat;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
 }
 </style>
